2be9b0e9fced81a9e22a6094c4c2a1c184e79a20
B0212977 <b0212977@B0212977s-MacBook-Pro.local>
2020-06-16 18:22:52 +0530

    first commit

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t amitairtel09/multi-client:latest -t amitairtel09/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t amitairtel09/multi-server:latest -t amitairtel09/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t amitairtel09/multi-worker:latest -t amitairtel09/multi-worder:$SHA -f ./worker/Dockerfile ./worker
+
+docker push amitairtel09/multi-client:latest
+docker push amitairtel09/multi-server:latest
+docker push amitairtel09/multi-worker:latest
+
+docker push amitairtel09/multi-client:$SHA
+docker push amitairtel09/multi-server:$SHA
+docker push amitairtel09/multi-worker:$SHA
+
+kubectl delete -f k8s
+kubectl set image deployments/server-deployment server=amitairtel09/multi-server:$SHA
+kubectl set image deployments/client-deployment server=amitairtel09/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=amitairtel09/multi-worker:$SHA
